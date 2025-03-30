4d55a386291f245d5f8b3a0b276a7a3ed88b7e51
Gabriel Ccama <gabriel.ccama@tecsup.edu.pe>
2025-03-30 16:41:39 -0500

    Added PATCH and DELETE endpoints in TaskController, and created DTO for PATCH

--- ToDoApi/Controllers/TaskController.cs
+++ ToDoApi/Controllers/TaskController.cs
@@ -7,6 +7,30 @@ namespace ToDoApi.Controllers;
 [Route("api/[controller]")]
 public class TaskController : ControllerBase
 {
-   private List<TaskModel> Tasks = new List<TaskModel>();
-
+   private static List<TaskModel> Tasks = new List<TaskModel>();
+   
+   // PATCH - uso de DTO
+   [HttpPatch("{id}")]
+   public IActionResult Patch([FromRoute] int id, [FromBody] TaslModelPatchDTO updates)
+   {
+      var task = Tasks.FirstOrDefault(t => t.Id == id);
+      if (task == null)
+         return NotFound(new { message = "Task not found" });
+      
+      if (updates.Status != null)
+         task.Status = updates.Status;
+      
+      return Ok(new { message = "Task patched", task });
+   }
+   
+   // DELETE
+   [HttpDelete("{id}")]
+   public IActionResult Delete([FromRoute] int id)
+   {
+      var task = Tasks.FirstOrDefault(t => t.Id == id);
+      if (task == null)
+         return NotFound(new { message = "Task not found" });
+      Tasks.Remove(task);
+      return Ok(new { message = "Task removed", Tasks });
+   }
 }
